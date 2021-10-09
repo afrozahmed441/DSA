2ec36e7c40db3eafcc96ce1c50d111cc1dd5456c
Afroz <afrozahmed441@gmail.com>
2021-10-09 21:18:22 +0530

    Single Linked List header file structure of class

---
+++ LinkedList/singleLinkedList/singleLinkedList.h
@@ -0,0 +1,28 @@
+#ifndef _SINGLELINKEDLIST_
+#define _SINGLELINKEDLIST_
+
+#include <iostream>
+
+struct node {
+    int data;
+    node *next;
+    node(int x) { 
+        data = x;
+        next = NULL; 
+    }
+};
+
+class singleLinkedList{
+    node *head;
+
+public:
+    singleLinkedList();
+    node *createNewNode(int);
+    void insertAtBegin(int);
+    void insertAtEnd(int);
+    int deleteAtBegin();
+    int deleteAtEnd();
+    void printList();
+};
+
+#endif
